3210467b68fdea83e20e57a7e6baee6275a51248
Manuel Arns <manuel.arns@icloud.com>
2021-05-04 13:31:13 +0200

    bug

--- eval.py
+++ eval.py
@@ -68,10 +68,10 @@ def compress_dataset(properties):
         if 'img1.png' in files:
             len_dataset_files += 1
 
-    print('...with HiFiC models:' + ''.join(properties.models))
-    print('...with interpolation methods:' + ''.join(properties.interpolation_methods))
-    print('...with interpolation depths:' + ''.join(map(str, properties.interpolation_depths)))
-    print('...number sequences in dataset:' + str(len_dataset_files))
+    print('...with HiFiC models:', ''.join(properties.models))
+    print('...with interpolation methods:', ''.join(properties.interpolation_methods))
+    print('...with interpolation depths:', ''.join(map(str, properties.interpolation_depths)))
+    print('...number sequences in dataset:', str(len_dataset_files))
     
     compression_results = np.zeros((len_models, len_interpolation_depths, len_dataset_files))
     process_steps = len_models * len_interpolation_depths * len_dataset_files
