0833eef1b2360ead5103b4cd6c7029d71389a43c
Manuel Arns <manuel.arns@icloud.com>
2021-05-04 13:33:56 +0200

    test

--- eval.py
+++ eval.py
@@ -65,6 +65,11 @@ def compress_dataset(properties):
     # count sequences
     len_dataset_files = 0
     for root, dirs, files in os.walk(properties.dataset_dir):
+        print()
+        print(root)
+        print(dirs)
+        print(files)
+        print()
         if 'img1.png' in files:
             len_dataset_files += 1
 
